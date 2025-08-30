eeb80cf64657fa532ccbe20bf6cfd5a7a74c2886
Tim Trainor <timjtrainor@trainoronline.com>
2025-08-29 23:19:09 -0700

    feat(frontend): add hash router for landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,13 @@
-export default function App() {
+import { Routes, Route } from 'react-router-dom';
+
+function Home() {
   return <h1>Hello, Trainium!</h1>;
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
